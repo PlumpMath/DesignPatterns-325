eb2ca3e576d789a80e37b76399033e54d0c5b35d
Sagar <sag.sahay@gmail.com>
2014-03-13 21:28:30 +0000

    Initial commit for decorator pattern

---
+++ DecoratorPattern/Beverage.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorPattern
+{
+   public abstract class Beverage
+   {
+      protected string description = "Unknown beverage";
+
+      public virtual string getDescription ()
+      {
+         return description;
+      }
+
+      public abstract double cost ();
+   }
+
+   public abstract class CondimentDecorator : Beverage
+   {
+     //public abstract string getDescription ();
+   }
+
+   public class Esprresso : Beverage
+   {
+      public Esprresso ()
+      {
+         description = "Espresso";
+      }
+
+      public override double cost ()
+      {
+         return 1.99;
+      }
+   }
+
+   public class HouseBlend : Beverage
+   {
+      public HouseBlend ()
+      {
+         description = "House Blend Coffee";
+      }
+
+      public override double cost ()
+      {
+         return .89;
+      }
+   }
+
+   public class Mocha : CondimentDecorator
+   {
+      Beverage beverage;
+
+      public Mocha ( Beverage beverage )
+      {
+         this.beverage = beverage;
+      }
+
+      public override string getDescription ()
+      {
+         return beverage.getDescription() + ", Mocha";
+      }
+
+      public override double cost ()
+      {
+         return .20 + beverage.cost();
+      }
+   }
+
+   public class Soy : CondimentDecorator
+   {
+      Beverage beverage;
+
+      public Soy ( Beverage beverage )
+      {
+         this.beverage = beverage;
+      }
+
+      public override string getDescription ()
+      {
+         return beverage.getDescription() + ", Soy";
+      }
+
+      public override double cost ()
+      {
+         return .20 + beverage.cost();
+      }
+   }
+
+   public class Whip : CondimentDecorator
+   {
+      Beverage beverage;
+
+      public  Whip ( Beverage beverage )
+      {
+         this.beverage = beverage;
+      }
+
+      public override string getDescription ()
+      {
+         return beverage.getDescription() + ", Whip";
+      }
+
+      public override double cost ()
+      {
+         return .20 + beverage.cost();
+      }
+   }
+
+}
---
+++ DecoratorPattern/StarBuzzCoffee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorPattern
+{
+   public class StarBuzzCoffee
+   {
+      public static void Main(string[] args)
+      {
+         Beverage beverage=new Esprresso();
+         Console.WriteLine(beverage.getDescription() + "$" +beverage.cost());
+
+         Beverage beverage2 = new HouseBlend();
+         beverage2=new Mocha(beverage2);
+         beverage2=new Mocha(beverage2);
+         beverage2=new Whip(beverage2);
+
+         Console.WriteLine(beverage2.getDescription() +"$"+ beverage2.cost());
+
+         Console.ReadLine();
+      }
+   }
+}
